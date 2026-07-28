10d6ac0a2798d5e17e746651af72be323c622107
Cotrimjr <cotrimjunior25@gmail.com>
2026-07-28 12:45:51 -0300

    Adicionar arquivos de projeto.

---
+++ VIImpact.API/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VIImpact.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            Status = "Online",
+            Project = "VI Impact",
+            Version = "1.0.0"
+        });
+    }
+}
